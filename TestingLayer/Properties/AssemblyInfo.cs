d73cc3e29c10a4337ce7036ec121736716897dd8
Justin Davis <JustinDavisA@Gmail.com>
2019-09-09 05:27:00 -0400

    Added testing layer

---
+++ TestingLayer/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestingLayer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestingLayer")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0a508179-5f4e-4758-9b51-6f17cb601911")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestingLayer/SimulationLogicTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestingLayer
+{
+    [TestClass]
+    public class SimulationLogicTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
